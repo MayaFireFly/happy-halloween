2c56a122555025a3ba280cca9cb121aa18c41156
Maya Tetenkina <maya_isb@mail.ru>
2020-02-10 11:17:55 +0500

    Added Loading and styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import './App.css';
 import Timer from '../Timer/Timer';
 import Dashboard from '../Dashboard/Dashboard';
+import Loading from '../Loading/Loading';
 import axios from 'axios';
 import {setTasks} from '../TasksContext';
 
 const App = () => {  
   const [timeTD, setTimeTD] = useState('');
   let flag = false;
-  const [dash, setDash] = useState(<Dashboard flag={flag}/>);
+  const [content, setContent] = useState(<div className='wrapper' id='app'><Loading/></div>);  
 
   useEffect(
     ()=>{
       if(!flag){
         axios.get('http://sol-tst.herokuapp.com/api/v1/tasks/').
           then((response)=>{
+
             if(response.status === 200){
-              setTasks(response.data.tasks);
-              if(response.data.endsAt != timeTD){
-                setTimeTD(response.data.endsAt);
-              }
-              flag = true;
-              setDash(<Dashboard flag={flag}/>);
+              setTasks(response.data.tasks);              
+              setTimeTD(response.data.endsAt);              
+              flag = true; 
+
             }else{
               alert(response.status + ' ' + response.statusText);
             }
@@ -33,17 +33,24 @@ const App = () => {
     },
     [flag]
   );
+
+  useEffect(
+    ()=>{
+      if(timeTD !== ''){
+        setContent(<div className='wrapper' id='app'>    
+          <div className='row row-timer'>
+            <Timer timeTillDate={timeTD}/>
+          </div>
+          <div className='row'>
+            <Dashboard flag={flag}/>
+          </div>                      
+        </div>);
+      }      
+    },
+    [timeTD]
+  );
   
-  return <div className='wrapper' id='app'>
-    
-    <div className='row row-timer'>
-      <Timer timeTillDate={timeTD}/>
-    </div>
-    <div className='row'>
-      {dash}
-    </div> 
-            
-  </div>;
+  return <div>{content}</div>;
 };  
 
 export default App;
